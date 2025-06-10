87c3b0efb75d36541966f88838dc8056c6ebac7a
dongmin-dev <dongminlee.0711@gmail.com>
2025-06-11 07:00:50 +0900

    feat(frontend): add and style Header component and integrate into app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header"; // Import the Header
 import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
@@ -7,12 +9,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* We will add a Header component here later */}
+        <Header /> {/* Add the Header component here */}
         <main>
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            {/* Other pages like the flight search will be added here later */}
-            {/* <Route path="/search" element={<SearchPage />} /> */}
           </Routes>
         </main>
       </div>
